5f05adec1b5ba1cc9a0f6d3dda54f558f1b151af
Андрей <andybro17@yandex.ru>
2022-06-06 14:41:07 +0200

    второй комит

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -8,7 +8,7 @@ public class Main {
         byte orangesWeight = 2;
         byte meatWeight = 1;
         byte potatoesWeight = 3;
-        double peppersWeight = 0.5;
+        double peppersWeight = 0.4;
 
         double productsWeight = bananasWeight + orangesWeight + meatWeight + potatoesWeight + peppersWeight;
         System.out.println("Общий вес продуктов " + productsWeight + " кг");
